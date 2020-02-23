d1f6e6acedff1e012939bfe5483597b6f4cbf9b6
jetlingkarthik <37239970+jetlingkarthik@users.noreply.github.com>
2020-02-23 12:23:01 +0530

    Ascii_to_Character.py

---
+++ Ascii_to_Character.py
@@ -0,0 +1,4 @@
+#Ascii value to character.
+Ascii_val = int(input("Enter the Ascii value to find character : "))
+Character = chr(Ascii_val)
+print(f'The Character of {Ascii_val} is : [ {Character} ]')
